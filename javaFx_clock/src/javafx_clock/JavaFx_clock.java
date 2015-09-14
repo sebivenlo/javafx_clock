0422c4ef5373a51c88e0e37a28f7b73a7f46107b
Maximilian Walter <ko08ok@gmail.com>
2015-09-14 13:00:21 +0200

    +animation example

--- javaFx_clock/src/javafx_clock/JavaFx_clock.java
+++ javaFx_clock/src/javafx_clock/JavaFx_clock.java
@@ -5,13 +5,26 @@
  */
 package javafx_clock;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+import javafx.animation.AnimationTimer;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.effect.Lighting;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 /**
  *
@@ -19,28 +32,77 @@ import javafx.stage.Stage;
  * 
  */
 public class JavaFx_clock extends Application {
-    
+        //main timeline
+    private Timeline timeline;
+    private AnimationTimer timer;
+ 
+    //variable for storing actual frame
+    private Integer i=0;
+ 
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
+    public void start(Stage stage) {
+    Group p = new Group();
+        Scene scene = new Scene(p);
+        stage.setScene(scene);
+        stage.setWidth(500);
+        stage.setHeight(500);
+        p.setTranslateX(80);
+        p.setTranslateY(80);
+ 
+        //create a circle with effect
+        final Circle circle = new Circle(20,  Color.rgb(156,216,255));
+        circle.setEffect(new Lighting());
+        //create a text inside a circle
+        final Text text = new Text (i.toString());
+        text.setStroke(Color.BLACK);
+        //create a layout for circle with text inside
+        final StackPane stack = new StackPane();
+        stack.getChildren().addAll(circle, text);
+        stack.setLayoutX(30);
+        stack.setLayoutY(30);
+ 
+        p.getChildren().add(stack);
+        stage.show();
+ 
+        //create a timeline for moving the circle
+        timeline = new Timeline();
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.setAutoReverse(true);
+ 
+//You can add a specific action when each frame is started.
+        timer = new AnimationTimer() {
             @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+            public void handle(long l) {
+                Calendar c = Calendar.getInstance();
+                String s = c.get(Calendar.HOUR)+" : "+c.get(Calendar.MINUTE)+" : "+c.get(Calendar.SECOND)+": "+c.get(Calendar.MILLISECOND); ;
+                text.setText(s);
+                i++;
             }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+ 
+        };
+ 
+        //create a keyValue with factory: scaling the circle 2times
+        KeyValue keyValueX = new KeyValue(stack.scaleXProperty(), 2);
+        KeyValue keyValueY = new KeyValue(stack.scaleYProperty(), 2);
+ 
+        //create a keyFrame, the keyValue is reached at time 2s
+        Duration duration = Duration.millis(1000);
+        //one can add a specific action when the keyframe is reached
+        EventHandler onFinished = (EventHandler<ActionEvent>) (ActionEvent t) -> {
+            stack.setTranslateX(java.lang.Math.random()*200-100);
+            //reset counter
+            i = 0;
+    };
+ 
+  KeyFrame keyFrame = new KeyFrame(duration, onFinished , keyValueX, keyValueY);
+ 
+        //add the keyframe to the timeline
+        timeline.getKeyFrames().add(keyFrame);
+ 
+        timeline.play();
+        timer.start();
     }
+ 
 
     /**
      * @param args the command line arguments
